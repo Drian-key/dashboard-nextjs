bbc9c4eb184ec7288f81a148c786ea37e4540e90
Chopper <riskidrian1@gmail.com>
2024-02-05 12:07:46 +0700

    add: metadata + change page welcome

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default function Page() {
   return (
@@ -20,7 +25,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 antialiased md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Drian.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
